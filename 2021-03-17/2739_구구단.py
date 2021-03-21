d53e5d6e508c81b3d19513d7ff48d9af35af9f25
Peyton <gasiorowicz10@gmail.com>
2021-03-21 22:40:24 +0900

    Add 20210321

---
+++ 2021-03-17/2739_구구단.py
@@ -0,0 +1,3 @@
+N = int(input())
+for num in range(1, 9):
+    print("%d * %d = %d" %(N, num, N * num))
---
+++ 2021-03-20/10872.py
@@ -0,0 +1,6 @@
+def factorial(N):
+    if N == 0: return 1
+    return factorial(N-1) * N
+
+N = int(input())
+print(factorial(N))
