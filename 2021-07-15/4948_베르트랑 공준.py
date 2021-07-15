87fac9d848da66e59b5682fc6ce7b6d9d3fe226f
Peyton Go <gasiorowicz10@gmail.com>
2021-07-15 20:50:21 +0900

    Add 20210715

--- 2021-07-15/4948_베르트랑 공준.py
+++ 2021-07-15/4948_베르트랑 공준.py
@@ -1,10 +1,19 @@
-for _ in range():
+def prime_list(n):
+    # 에라토스테네스의 체 초기화: N개 요소에 True설정(소수로 간주)
+    sieve = [True] * n
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i = sqrt(n)까지 검사
+    m = int(n ** 0.5)
+    for i in range(2, m+1):
+        if sieve[i] == True:  # i가 소수인 경우
+            for j in range(i+i, n, i): # i 이후 i의 배수들을 False로 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]
+
+while 1:
     n = int(input())
-    pm = 0
-    cnt = 0
-    for i in range(n+1, 2*n+1):
-        for j in range(1, i+1):
-            if i % j == 0:
-                cnt += 1
-        if cnt == 2: pm += 1
-    print(pm)
+    if n == 0: break
+    li = prime_list(2*n+1)
+    print(len([i for i in li if i > n]))
