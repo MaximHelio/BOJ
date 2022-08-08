b4b2274af6afdaef46e578d4d16fc5f4541d03ae
kknni <gasiorowicz10@gmail.com>
2022-08-08 20:50:09 +0900

    Add 20220808

--- 2022-08-07/2920. 음계.py
+++ 2022-08-07/2920. 음계.py
@@ -1,4 +1,27 @@
-arr = list(map(int, input().split()))
-for i in range(0, len(arr)):
-    if arr[i] < arr[i+1]: continue
-    
+arr = list(map(int, input().split(' ')))
+ascending = 0
+descending = 0
+mixed = 0
+for i in range(0, len(arr)-1):
+    if arr[i] < arr[i+1]: ascending += 1
+    elif arr[i] > arr[i+1]: descending += 1
+    else: mixed += 1
+
+if ascending > 0 and descending == 0: print("ascending")
+elif ascending ==0 and descending >0 : print("descending")
+else: print("mixed")
+
+
+
+a = list(map(int, input().split('')))
+ascending = True
+descending = True
+
+for i in range(0, 7):
+    if a[i] < a[i+1]: descending = False
+    elif a[i] > a[i+1]: ascending = False
+
+if ascending: print('ascending')
+elif descending: print('descending')
+else: print('mixed')
+
